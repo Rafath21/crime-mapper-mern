7ab112f9a3ae8001bedf9b8e0bedb0a7a82d21fe
Rafath21 <rafathmirza05@gmail.com>
2021-10-30 20:30:07 +0530

    Completed

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express=require("express");
 const app=express();
-require("dotenv").config({ path: "./config.env" });
 const cors=require("cors");
 const connectDB=require("./config/db");
 app.use(express.json())
-app.use(cors());
+if(process.env.NODE_ENV!=="PRODUCTION"){
+    require("dotenv").config({ path: "./config.env" });
+    app.use(cors());
+
+}
 connectDB();
-app.use(express.static('public'));
+//app.use(express.static('public'));
 app.use("/api/v1/addCrime",require("./routes/addRoute"));
 app.use("/api/v1/getCrime",require("./routes/getRoute"));
 
